234a94ecb816471d9302033c5259d3f35ad38fdc
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-04-05 20:36:46 +0530

    Done with the hero section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
+import {Merienda} from 'next/font/google'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Merienda({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
